0dab0e4d6fff634012dcb704c229d5a95e09135a
Zheng-kiki <1193083111@qq.com>
2026-03-05 13:30:16 +0800

    java后端仓库

---
+++ java/kiki/src/main/java/org/example/kiki/KikiApplication.java
@@ -0,0 +1,13 @@
+package org.example.kiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KikiApplication.class, args);
+    }
+
+}
---
+++ java/kiki/src/test/java/org/example/kiki/KikiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
